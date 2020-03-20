faf263a514a4c289afe50d61b01a7bd16dba51c8
lss98 <lss98@DESKTOP-BF2TG9S>
2020-03-20 19:31:48 +0900

    create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("*** Part-time job Management System Menu ***");
+		System.out.println(" 1. Add Part-time job");
+		System.out.println(" 2. Delete Part-time job");
+		System.out.println(" 3. Edit Part-time job");
+		System.out.println(" 4. View Part-time job");
+		System.out.println(" 5. Exit");
+		System.out.println("Select one number between 1-5");
+	}
+
+}
