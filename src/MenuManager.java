4950f9bd2890c33a57f0de9ebe202024b2769a5a
lss98 <lss98@DESKTOP-BF2TG9S>
2020-03-22 12:45:15 +0900

    Finish

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,12 +2,13 @@ public class MenuManager {
 
 	public static void main(String[] args) {
 		System.out.println("*** Part-time job Management System Menu ***");
+		System.out.println(" -- Select one number between 1-5 -- ");
 		System.out.println(" 1. Add Part-time job");
 		System.out.println(" 2. Delete Part-time job");
 		System.out.println(" 3. Edit Part-time job");
 		System.out.println(" 4. View Part-time job");
 		System.out.println(" 5. Exit");
-		System.out.println("Select one number between 1-5");
+
 	}
 
 }
